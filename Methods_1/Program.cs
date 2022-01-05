fafa89e66cde6770f585084b4f5a1511409aaef9
redknight222 <golubni4enko.a@yandex.ru>
2022-01-06 01:48:31 +0300

    Добавьте файлы проекта.

---
+++ Methods_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double S1 = CalcTriangleSquare(3, 4, 5);
+            double S2 = CalcTriangleSquare(6, 7, 8);
+            if (S1 > S2)
+            {
+                Console.WriteLine("triangle 1 Square more than 2");
+            }
+            else
+            {
+                Console.WriteLine("triangle 2 Square more than 1");
+            }
+            Console.ReadKey();
+        }
+        static double CalcTriangleSquare(int x, int y, int z)
+        {
+            double p = (x + y + z) / 2;
+            return Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+        }
+    }
+}
---
+++ Methods_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double S, V;
+            CalcCubeSquareVolume(3,out S, out V);
+            Console.WriteLine("Cube square ={0}", S);
+            Console.WriteLine("Cube volume ={0}", V);
+            Console.ReadKey();
+        }
+        static void CalcCubeSquareVolume(int a, out double S, out double V)
+        {
+            S = 6 * Math.Pow(a, 2);
+            V = Math.Pow(a, 3);
+        }
+    }
+}
